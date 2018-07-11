9b7cfc0cc945bda84f13185653a516bb1deaa4b3
reeeeo <reo.117.pri@gmail.com>
2018-07-11 15:08:23 +0800

    fix listController   - add swipe-delete   - adjust label length

--- pachira/ListViewController.swift
+++ pachira/ListViewController.swift
@@ -33,11 +33,22 @@ extension ListViewController: UITableViewDataSource {
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
     let idea = self.ideas[indexPath.row]
-    cell.textLabel?.text = idea.name
-    
-    cell.contentView.addSubview(getCellImageView(by: idea.pic1, height: cell.contentView.bounds.height, num: 1))
-    cell.contentView.addSubview(getCellImageView(by: idea.pic2, height: cell.contentView.bounds.height, num: 2))
+    let cellHeight = cell.contentView.bounds.height
+    cell.contentView.addSubview(getCellLabel(with: idea.name!, height: cellHeight))
+    cell.contentView.addSubview(getCellImageView(by: idea.pic1, height: cellHeight, num: 1))
+    cell.contentView.addSubview(getCellImageView(by: idea.pic2, height: cellHeight, num: 2))
     return cell
+  }
+  
+  private func getCellLabel(with name: String, height: CGFloat) -> UILabel {
+    let textLabel = UILabel(frame: CGRect(
+      x: 15.0,
+      y: 0.0,
+      width: self.view.bounds.size.width - (height * 2) - 20.0,
+      height: height))
+    textLabel.lineBreakMode = .byTruncatingTail
+    textLabel.text = name
+    return textLabel
   }
   
   private func getCellImageView(by id: Int16, height: CGFloat, num: Int) -> UIImageView {
@@ -54,5 +65,14 @@ extension ListViewController: UITableViewDataSource {
     let next = DetailViewController()
     next.idea = ideas[indexPath.row]
     self.navigationController?.pushViewController(next, animated: true)
+  }
+  
+  func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    if editingStyle == .delete {
+      let targetIdea = ideas[indexPath.row]
+      ideas.remove(at: indexPath.row)
+      tableView.deleteRows(at: [indexPath], with: .fade)
+      CDIdea.coreData.delete(targetIdea)
+    }
   }
 }
